8945c6f620531fa11288def106d34be219c018af
ArunWB <arun@workbetterindia.com>
2021-07-16 12:35:08 +0530

    Commit 4
    Comitted x 4

---
+++ story_content/user.js
@@ -0,0 +1,799 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5yTyg3f7lAp":
+        Script1();
+        break;
+      case "6TirtS5VPBW":
+        Script2();
+        break;
+      case "69pL3uhwtMQ":
+        Script3();
+        break;
+      case "63F3dCMA7Ry":
+        Script4();
+        break;
+      case "5wFZEj8XvsO":
+        Script5();
+        break;
+      case "6PCGb7jRnmC":
+        Script6();
+        break;
+      case "5cOGcTNcCeK":
+        Script7();
+        break;
+      case "5eUXVC8YYgu":
+        Script8();
+        break;
+      case "5c5BZcKRn6u":
+        Script9();
+        break;
+      case "6HXpABgCgC7":
+        Script10();
+        break;
+      case "6CaFQLyp2tM":
+        Script11();
+        break;
+      case "5ZtMB6U0IrJ":
+        Script12();
+        break;
+      case "6PiIVqWb58S":
+        Script13();
+        break;
+      case "6VVKxYvwO3P":
+        Script14();
+        break;
+      case "6NgGN3hIGSD":
+        Script15();
+        break;
+      case "6d5n1LOwz6v":
+        Script16();
+        break;
+      case "62CvFwZxU3F":
+        Script17();
+        break;
+      case "6nrgLPBpAAZ":
+        Script18();
+        break;
+      case "5m2p56mrlyg":
+        Script19();
+        break;
+      case "5ylUWdnlmhd":
+        Script20();
+        break;
+      case "6kp71kEFdDk":
+        Script21();
+        break;
+      case "6oyX7mu16SE":
+        Script22();
+        break;
+      case "6YABdRU9z9J":
+        Script23();
+        break;
+      case "5WCkluzF7mz":
+        Script24();
+        break;
+      case "5pTfzAa9P43":
+        Script25();
+        break;
+      case "6ZE61i7dVs0":
+        Script26();
+        break;
+      case "6TBmNw2I0fi":
+        Script27();
+        break;
+      case "6a1YRsPQYjd":
+        Script28();
+        break;
+      case "6UVq3FEXLiu":
+        Script29();
+        break;
+      case "5c8en7mdwTL":
+        Script30();
+        break;
+      case "6UdHO8DVKlt":
+        Script31();
+        break;
+      case "5ivKgK5zpkK":
+        Script32();
+        break;
+      case "6PwC0l2iQ7C":
+        Script33();
+        break;
+      case "6DjANYeDCgN":
+        Script34();
+        break;
+      case "6Dhqxj5y87Q":
+        Script35();
+        break;
+      case "6ey5D5QYUVh":
+        Script36();
+        break;
+      case "5iEWORm4ddz":
+        Script37();
+        break;
+      case "5jSoBCNDuVy":
+        Script38();
+        break;
+      case "5kMQ4Qf0rIg":
+        Script39();
+        break;
+      case "5V7Cx3jLvfR":
+        Script40();
+        break;
+      case "6HMeGSLO3VD":
+        Script41();
+        break;
+      case "5yiJ7RvJrhi":
+        Script42();
+        break;
+      case "6rVCO9Ux1VZ":
+        Script43();
+        break;
+      case "6o2Au3nhtIX":
+        Script44();
+        break;
+      case "6LsLhJmp5UJ":
+        Script45();
+        break;
+      case "5itUcI7KS5u":
+        Script46();
+        break;
+      case "60UokaFFTt2":
+        Script47();
+        break;
+      case "6i0RjpTOPWh":
+        Script48();
+        break;
+      case "5ZHA3JFtuqG":
+        Script49();
+        break;
+      case "6d5wdYA8s5H":
+        Script50();
+        break;
+      case "5sx4b6SevQ6":
+        Script51();
+        break;
+      case "6DPLWnmsg5Q":
+        Script52();
+        break;
+      case "5nVSCFjDOVn":
+        Script53();
+        break;
+      case "6SoXAbGxwYv":
+        Script54();
+        break;
+      case "6lzgvWx7gUd":
+        Script55();
+        break;
+      case "6NLRww2Ksct":
+        Script56();
+        break;
+      case "5kk6MsGW92X":
+        Script57();
+        break;
+      case "679Q7EOn80H":
+        Script58();
+        break;
+      case "6LYjBG2cz1G":
+        Script59();
+        break;
+      case "5ZQOOj07nCY":
+        Script60();
+        break;
+      case "5vsuBPahdiD":
+        Script61();
+        break;
+      case "5W33bQnxe6D":
+        Script62();
+        break;
+      case "6Chn5PimSUT":
+        Script63();
+        break;
+      case "6Di5zbqJLPB":
+        Script64();
+        break;
+      case "687knFWGGgZ":
+        Script65();
+        break;
+      case "5wnyovPt4Fc":
+        Script66();
+        break;
+      case "6IVT6zxRSGw":
+        Script67();
+        break;
+      case "5zYj3P52wcW":
+        Script68();
+        break;
+      case "6VtQdzhcWUm":
+        Script69();
+        break;
+      case "6Ie0E9nfAPJ":
+        Script70();
+        break;
+      case "6eW6ExG1Zok":
+        Script71();
+        break;
+      case "5vPyq6veWXd":
+        Script72();
+        break;
+      case "6nZZsgJOn3Y":
+        Script73();
+        break;
+      case "61vGULpP2P8":
+        Script74();
+        break;
+      case "5mWJI7HIdVc":
+        Script75();
+        break;
+      case "68HwRLwdVal":
+        Script76();
+        break;
+      case "6fC4bhBClTq":
+        Script77();
+        break;
+      case "61tbhyKRQyD":
+        Script78();
+        break;
+      case "5fbyxN7VAFk":
+        Script79();
+        break;
+      case "6HLO9Vn7V6i":
+        Script80();
+        break;
+      case "5mecvNOYlVs":
+        Script81();
+        break;
+      case "6AoB7LdiDKD":
+        Script82();
+        break;
+      case "62F73fRRggb":
+        Script83();
+        break;
+      case "5l5oiB82Qyg":
+        Script84();
+        break;
+      case "5sbK9J1ciV4":
+        Script85();
+        break;
+      case "6JGHQVXNEU1":
+        Script86();
+        break;
+      case "6NGaZ7OJBhZ":
+        Script87();
+        break;
+      case "5qSNpSfAZJo":
+        Script88();
+        break;
+  }
+}
+
+function Script1()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script2()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script3()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script4()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script5()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script6()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script7()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script8()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script9()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script10()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script11()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script12()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script13()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script14()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script15()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script16()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script17()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script18()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script19()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script20()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script21()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script22()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script23()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script24()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script25()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script26()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script27()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script28()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script29()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script30()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script31()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script32()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script33()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script34()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script35()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script36()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script37()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script38()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script39()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script40()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script41()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script42()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script43()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script44()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script45()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script46()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script47()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script48()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script49()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script50()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script51()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script52()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script53()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script54()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script55()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script56()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script57()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script58()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script59()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script60()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script61()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script62()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script63()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script64()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script65()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script66()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script67()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script68()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script69()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script70()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script71()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script72()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script73()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script74()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script75()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script76()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script77()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script78()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script79()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script80()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script81()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script82()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script83()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script84()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script85()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script86()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script87()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script88()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
